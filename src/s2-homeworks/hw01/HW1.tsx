79d7ab4700ba0f6edb7cf3dc4f71d04c6e9966a6
Павел Володченко <pavelvolodcenko@Pavels-MacBook-Air.local>
2025-04-01 13:47:20 +0100

    first

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,17 +14,26 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
-
+export type MessageType = {
+    id:number
+    user:{
+        avatar:string,
+        name:string
+    }
+    message:{
+        text:string
+        time:string
+    }
+}
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'Pavel',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: 'Hello, she didn’t do anything and rested all day, how are you?', // можно менять
         time: '22:00', // можно менять
     },
 }
@@ -32,7 +41,7 @@ export const friendMessage0: MessageType = {
     id: 100,
     user: {
         avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        name: 'Alex', // можно менять
     },
     message: {
         text: 'зеркальное сообщение для тренировки css', // можно менять
@@ -46,7 +55,7 @@ const HW1 = () => {
             <div className={s2.hwTitle}>Homework #1</div>
             <div className={s2.hw}>
                 {/*проверка отображения (не менять)*/}
-                <div>
+                <div className={s2.messages}>
                     <Message message={message0} />
                     <FriendMessage message={friendMessage0} />
                 </div>
